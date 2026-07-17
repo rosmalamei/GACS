bdce432fbec8847b2674b84ed96d807a943e24cf
rosmalamei <rosmalamei001@gmail.com>
2026-07-17 18:58:22 +0700

    nodejs.sh

--- nodejs.sh
+++ nodejs.sh
@@ -27,7 +27,7 @@ check_node_version() {
 
 if ! check_node_version; then
     echo -e "${GREEN}================== Menginstall NodeJS ==================${NC}"
-    curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+    curl -fsSL https://deb.nodesource.com/setup_22.x | sudo bash -
     sudo apt-get install -y nodejs
     cd
     rm -r GACS
